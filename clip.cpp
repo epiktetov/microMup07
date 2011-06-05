epiktetov/microMup07

commit c6e861c1e734137ceaa74f3c8893ed2de306def6

MiScTwin::Text(tchar) optimization
Text drawing optimized to reduce context switching (which is very expensive
operation on Mac), plus some micro-optimization that did not help much, but
apparently did not hurt either

tx.c
@@ -277,9 +277,10 @@ small aftotc (const char *orig, int len, tchar *dest_buf)
       *dest_buf++ = ((tchar)y << 6) | z | attr;
     }
     else if ((c & 0x80) != 0)
-     { if (c & 0x40) { *dest_buf++ = (ctotc(c) + 0x350) | attr | AT_BADCHAR; }
-       else          { *dest_buf++ = (ctotc(c) +  0x60) | attr | AT_BADCHAR; }}
-    else             { *dest_buf++ =  ctotc(c)          | attr;               }
+     { if (c & 0x40)  { *dest_buf++ = (ctotc(c)+0x350) | attr | AT_BADCHAR; }
+       else           { *dest_buf++ = (ctotc(c)+ 0x60) | attr | AT_BADCHAR; }}
+    else if (c < ' ') { *dest_buf++ = (ctotc(c)+ '@' ) | attr | AT_TAB;      }
+    else              { *dest_buf++ =  ctotc(c)        | attr;               }
     ltc++;
   }
   return ltc;
@@ -289,7 +290,7 @@ small aftotc (const char *orig, int len, tchar *dest_buf)
 small tctoaf (tchar *orig, int len, char *dest_buf)
 {
   char attr = 0, cattr; int laf, itc, j;
-  tchar  c, cna;
+  tchar  c, csa;
   for (itc = laf = 0; itc < len && laf < MAXLUP; itc++, laf++) {
     c = orig[itc];
     if ((cattr = (char)((c & AT_IN_FILE) >> 16)) != attr) {
@@ -297,43 +298,40 @@ small tctoaf (tchar *orig, int len, char *dest_buf)
       if (laf < MAXLUP-2) { *dest_buf++ = ACPR;
                             *dest_buf++ = ACPR2 | attr; laf += 2; }
     }
-    cna = c & AT_CHAR; /* = character sans attributes */
+    csa = c & AT_CHAR; /* = character sans attributes */
     if (c & AT_TAB) { 
-      for (j = itc+1; j < len && (j % TABsize) != 0; j++)
-        if ((char)orig[j] != ' ') goto vanilla_char;
-      /*  ^
-       * Replacing space with AT_TAB back to TAB only works if all tchars up to
-       *  the next tab-stop are spaces, otherwise attribute is silently ignored
-       */
-      *dest_buf++ = TAB; itc = j - 1; continue;
+      if (csa != ' ') *dest_buf++ = (char)(csa - '@');  /* control character */
+      else {
+        for (j = itc+1; j < len && (j % TABsize) != 0
+                                && (char)orig[j] == ' '; j++) ;
+        *dest_buf++ = TAB;
+        itc = j-1; /* skip all spaces up to next TAB stop or first non-space */
+    } }
+    else if (c & AT_BADCHAR) {
+           if ( 0xE0 <= csa && csa <= 0x11F) *dest_buf++ = (char)(csa -  0x60);
+      else if (0x410 <= csa && csa <= 0x44F) *dest_buf++ = (char)(csa - 0x350);
     }
-    if (c & AT_BADCHAR) {
-           if ( 0xE0 <= cna && cna <= 0x11F) *dest_buf++ = (char)(cna -  0x60);
-      else if (0x410 <= cna && cna <= 0x44F) *dest_buf++ = (char)(cna - 0x350);
-      else goto vanilla_char;                                         continue;
-    }
-vanilla_char: 
 #ifdef notdef
-    if (cna > 0xFFFF && laf < MAXLUP-3) {     /* 000wwwxx xxxxyyyy yyzzzzzz */
+    if (csa > 0xFFFF && laf < MAXLUP-3) {     /* 000wwwxx xxxxyyyy yyzzzzzz */
       laf += 3;                                              /* plus 3 byte */ 
-      *dest_buf++ = (char)(0xF0 | ((cna & 0x1C0000) >> 18)); /* -> 11110www */
-      *dest_buf++ = (char)(0x80 | ((cna & 0x03F000) >> 12)); /*    10xxxxxx */
-      *dest_buf++ = (char)(0x80 | ((cna & 0x000FC0) >>  6)); /*    10yyyyyy */
-      *dest_buf++ = (char)(0x80 |  (cna & 0x00003F)       ); /*    10zzzzzz */
+      *dest_buf++ = (char)(0xF0 | ((csa & 0x1C0000) >> 18)); /* -> 11110www */
+      *dest_buf++ = (char)(0x80 | ((csa & 0x03F000) >> 12)); /*    10xxxxxx */
+      *dest_buf++ = (char)(0x80 | ((csa & 0x000FC0) >>  6)); /*    10yyyyyy */
+      *dest_buf++ = (char)(0x80 |  (csa & 0x00003F)       ); /*    10zzzzzz */
     }
 #endif
-    if (cna > 0x7FF && laf < MAXLUP-2) {          /* 0000 xxxxyyyy yyzzzzzz */
+    else if (csa > 0x7FF && laf < MAXLUP-2) {     /* 0000 xxxxyyyy yyzzzzzz */
       laf += 2;                                              /* plus 2 byte */ 
-      *dest_buf++ = (char)(0xE0 | ((cna & 0x00F000) >> 12)); /* -> 1110xxxx */
-      *dest_buf++ = (char)(0x80 | ((cna & 0x000FC0) >>  6)); /*    10yyyyyy */
-      *dest_buf++ = (char)(0x80 |  (cna & 0x00003F)       ); /*    10zzzzzz */
+      *dest_buf++ = (char)(0xE0 | ((csa & 0x00F000) >> 12)); /* -> 1110xxxx */
+      *dest_buf++ = (char)(0x80 | ((csa & 0x000FC0) >>  6)); /*    10yyyyyy */
+      *dest_buf++ = (char)(0x80 |  (csa & 0x00003F)       ); /*    10zzzzzz */
     }
-    else if (cna > 0x7F && laf < MAXLUP-1) {           /* 00000yyy yyzzzzzz */
+    else if (csa > 0x7F && laf < MAXLUP-1) {           /* 00000yyy yyzzzzzz */
       laf += 1;                                              /* plus 1 byte */ 
-      *dest_buf++ = (char)(0xC0 | ((cna & 0x0007C0) >>  6)); /* -> 110yyyyy */
-      *dest_buf++ = (char)(0x80 |  (cna & 0x00003F)       ); /*    10zzzzzz */
+      *dest_buf++ = (char)(0xC0 | ((csa & 0x0007C0) >>  6)); /* -> 110yyyyy */
+      *dest_buf++ = (char)(0x80 |  (csa & 0x00003F)       ); /*    10zzzzzz */
     }
-    else *dest_buf++ = (char)cna;
+    else *dest_buf++ = (char)csa;
   }
   while (laf && *(--dest_buf) == ' ') laf--; /* remove trailing spaces */
   return laf;
@@ -358,8 +356,8 @@ tchar *TxInfo (wnd *w, large y, int *pl)      /* used for repaint in vip.cpp */
       for (i = 0; i < TXT_TempMARK; i++) {
         if (t->txmarky[i] == y) {
           if (len < w->wsw-1) tcbuf[len++] = ' ';
-                              tcbuf[len++] =      txFlags[i];
-                              tcbuf[len++] = ' '|(txFlags[i] & AT_ALL); break;
+                              tcbuf[len++] =           txFlags[i];
+                              tcbuf[len++] = ' '|tATTR(txFlags[i]); break;
   } } } }
   if (len < tcbuflen) blktspac(tcbuf+len, tcbuflen - len);
   *pl = tcbuflen = len;                      return tcbuf;

le.c
@@ -347,12 +347,12 @@ static void laSetOption (int option)
     if (leOptMode & LeARG_IGNORECASE) imode = "cs";
     else                              imode = "ic";
     leOptMode ^= LeARG_IGNORECASE;
-    Lebuf[4] = (Lebuf[4] & AT_ALL) + imode[0];
-    Lebuf[5] = (Lebuf[5] & AT_ALL) + imode[1]; return;
+    Lebuf[4] = tATTR(Lebuf[4]) + imode[0];
+    Lebuf[5] = tATTR(Lebuf[5]) + imode[1]; return;
   }
   leOptMode = option | (leOptMode & LeARG_IGNORECASE);
-  Lebuf[1] = (Lebuf[1] & AT_ALL) + smode[0];
-  Lebuf[2] = (Lebuf[2] & AT_ALL) + smode[1];
+  Lebuf[1] = tATTR(Lebuf[1]) + smode[0];
+  Lebuf[2] = tATTR(Lebuf[2]) + smode[1];
   for (i = Lxlm; i < Lleng; i++)
     if (!(Lebuf[i] & AT_SUPER))
       Lebuf[i] = Lattr | (Lebuf[i] & ~(AT_PROMPT|AT_REGEX));

le.h
@@ -45,7 +45,7 @@ void LenterARG(tchar *buf, int *bufLen, /* buffer for argument & its length  */
 #define LeARG_IGNORECASE 8 /* Ctrl+I i ignore case toggle (default is 'yes') */
 extern int leOptMode; /* options corresponding to final result is found here */
 /*- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -*/
-#define tcharIsBlank(tc) ((tc & ~AT_ALL) == (tchar)' ')
+#define tcharIsBlank(tc) ((short)(tc & AT_CHAR) == ' ')
 
 extern int  char2tcharsX(const  char *orig, tchar *buffer, tchar attr);
 extern int  char2tchars (const  char *orig, tchar *buffer);

mic.h
@@ -29,7 +29,7 @@ typedef int   BOOL;
 #define NIL      ((void*)0) /* указатель (same as standard NULL but shorter) */
 /*- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -*/
 typedef long tchar;                           /* Unicode символ с атрибутами */
-#define AT_ALL     0x7FFF0000
+#define tATTR(x)(x&0x7FFF0000)
 #define AT_CHAR    0x0000FFFF /* собственно Unicode символ (use only 16-bit) */
 #define AT_BOLD    0x00010000 /* Bold                               == KxTS  */
 #define AT_PROMPT  0x00020000 /* prompts text (def. dark blue text) == KxBLK */

mim.cpp
@@ -688,37 +688,37 @@ void MiScTwin::Text (QPainter& dc, int x, int y, int attr, QString text)
   dc.drawText(Tx2qtX(x), Ty2qtY(y)+fontBaseline, text);
 }
 //- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-inline QChar vChar (tchar tc) // convert chars below space into printable ones
-{ 
-  if (tc & AT_QUOTE) {
-    switch (tc & (AT_CHAR|AT_QOPEN|AT_QCLOSE)) {
-    case AT_QOPEN |'\'': return QChar(0x2018); // ‘
-    case AT_QCLOSE|'\'': return QChar(0x2019); // ’
-    case AT_QOPEN | '"': return QChar(0x201C); // “
-    case AT_QCLOSE| '"': return QChar(0x201D); // ”
-    default:             return int(tc & AT_CHAR);
-  } }
-  else {
-    tchar tcc = (tc & AT_CHAR);  if (tcc  >  ' ') return int(tcc);
-                            else if (tcc  <  ' ') return int(tcc)+'@';
-                            else if (tc & AT_TAB) return QChar(0x0BB); // »
-                            else                  return ' ';
-} }
-inline tchar vAttr (tchar tc) 
-{ 
-  tchar attr = tc & AT_ALL; return ((tc & AT_CHAR) < ' ') ? AT_TAB|attr : attr;
-}
-//- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 void MiScTwin::Text (QPainter& dc, int x, int y, tchar *tp, int len)
 {
-  tchar *tpbeg, *tpend = tp + len, attr;
-  QChar str[MAXLPAC], *p;
-  for (tpbeg = tp; tp < tpend; tpbeg = tp) {  attr = vAttr(*tp);
-    p = str;
-    do { *p++ = vChar(*tp++); } while (tp < tpend && vAttr(*tp) == attr);
-    len = tp - tpbeg;
-    Text(dc, x, y, attr, QString(str, len)); x += len;
-} }
+  static QChar str [MAXLPAC]; tchar *tpend = tp + len, tc, tcc;
+         QChar *sf = str, qc; tchar tA = 1, tC;
+  while (tp < tpend) {
+    tc = *tp++;
+    tC = tATTR(tc);
+    tcc = (tc & AT_CHAR);
+    if (tc & AT_QUOTE) { tC &= ~(AT_QOPEN|AT_QCLOSE);
+           switch (tc & (AT_CHAR|AT_QOPEN|AT_QCLOSE)) {
+           case AT_QOPEN |'\'': qc.unicode() = 0x2018; break; // ‘
+           case AT_QCLOSE|'\'': qc.unicode() = 0x2019; break; // ’
+           case AT_QOPEN | '"': qc.unicode() = 0x201C; break; // “
+           case AT_QCLOSE| '"': qc.unicode() = 0x201D; break; // ”
+           default:             qc.unicode() = ushort(tc & AT_CHAR);
+    }      }
+    else if (tcc  >  ' ') qc.unicode() = ushort(tcc);
+    else if (tcc  <  ' ') qc.unicode() = ushort(tcc)+'@';
+    else if (tc & AT_TAB) qc.unicode() = 0x0BB; // »
+    else {                qc.unicode() = ' ';
+      if (((tA^tC) & (AT_INVERT|AT_MARKFLG|AT_BG_CLR)) == 0) tC = tA;
+    }
+    if (tC == tA) *sf++ = qc;
+    else {
+      if ((len = sf-str) > 0) {
+        Text(dc, x, y, tA, QString(str,len)); x += len;
+      }
+      sf = str; *sf++ = qc; tA = tC;
+  } }
+  if ((len = sf-str) > 0) Text(dc, x, y, tA, QString(str,len));
+}
 //-----------------------------------------------------------------------------
 void MiScTwin::keyPressEvent (QKeyEvent *event)
 {

commit 29b78637c7423963d5753625c17733cb87da76f5

Clipboard fix, much faster search
Fixed saving text to clipboard (broken by changes in c6e861c1e… commit),
search made about x5 times faster - at the price of not working with bold
attribute (corresponding comment added to help), "file enter" now replaces
"this file" special char correctly

clip.cpp
@@ -139,12 +139,12 @@ void cpreopen()
 /*- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -*/          
 static void toClipboard()
 {
-  CSclose();  TxTop(LCtxt); bool add_EOL = false; QString cbData;
+  CSclose();  TxTop(LCtxt); bool add_EOL = false; QString cbData; int len;
   while (!qTxBottom(LCtxt)) {
     if (add_EOL) cbData += (QChar)clipEOL;
-    Lleng = TxTRead(LCtxt, Lebuf); 
-    int dx = (Lebuf[0] == (tchar)CpSCH_NEW_LINE) ? 1 : 0;
-    cbData += tcs2qstr(Lebuf+dx, Lleng-dx); 
+    char *tp = TxGetLn(LCtxt, &len);
+    int dx = (*tp == CpSCH_NEW_LINE) ? 1 : 0;
+    cbData += QString::fromUtf8(tp+dx, len-dx);
     if (dx) cbData += (QChar)clipEOL;
     else              add_EOL = true; TxDown(LCtxt);
   }

micro.keys
@@ -369,6 +369,10 @@ ones (without selection, current character is converted and cursor moves right)
   Alt+Meta+ <up>   - convert current character/selection to uppercase
   Alt+Meta+<down>  - convert current character/selection to lowercase
   Ctrl+B           - add / clear "bold" attribute
+
+NOTE: "bold" attribute is marked by Unicode chars U+281 / U+280 (which are very
+unlikely to appear in regular text) and not compatible with anything.. not even
+with build-in MicroMir search engine. Use at your own risk, you've been warned!
 */
 k_(LE_CCUP,  mod_ALT+mod_META+Mk_UP,   0xf8913) /* буква -> прописная */
 k_(LE_CCDWN, mod_ALT+mod_META+Mk_DOWN, 0xf8915) /* буква -> строчная  */

synt.cpp
@@ -30,9 +30,10 @@ small SyntKnownLang (QString filename)
 //- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 small SyntSniffText (txt *text)
 {
-  TxTop(text);            if (qTxBottom(text)) return CLangNONE;
-  Lleng = TxTRead(text, Lebuf); if (Lleng < 3) return CLangNONE;
-  QString firstLine = tcs2qstr (Lebuf, Lleng);
+
+  int len;   TxTop(text); if (qTxBottom(text)) return CLangNONE;
+  char *tp = TxGetLn(text, &len); if (len < 3) return CLangNONE;
+  QString firstLine = QString::fromUtf8(tp, len);
   QRegExp binSpec("#!(?:/usr)?(?:/local)?(?:/bin/)?(\\w+)\\s*(.*)");
   QRegExp bash("(ba)?sh");
   if (!binSpec.exactMatch(firstLine)) return CLangNONE;

te.c
@@ -352,9 +352,9 @@ void tesPrepare()
 /*---------------------------------------------------------------------------*/
 void tesdown (void)
 {
-  int x = Tx; tesPrepare();
+  int x = Tx, len; tesPrepare();
   for(;;) {
-    Lleng = TxTRead(Ttxt, Lebuf); x = vipFind(Lebuf, Lleng, x+1, FALSE);
+    char *str = TxGetLn(Ttxt, &len); x = vipFind(str, len, x+1, FALSE);
     if (x < 0) {  
       if (qkbhin())       exc(E_KBREAK); TxDown(Ttxt);
       if (qTxBottom(Ttxt)) exc(E_SFAIL); // x == -1 here, exactly start of line
@@ -365,14 +365,14 @@ void tesdown (void)
 /*- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -*/
 void tesup(void) 
 {
-  int x = Tx; tesPrepare();
+  int x = Tx, len; tesPrepare();
   for(;;) {
     if (x < 1) { 
       if (qTxTop(Ttxt)) exc(E_SFAIL); TxUp(Ttxt);
       if (qkbhin())    exc(E_KBREAK);
     }
-    Lleng = TxTRead(Ttxt, Lebuf); 
-    x = vipFind(Lebuf, Lleng, (x < 0) ? Lleng : x-1, TRUE);
+    char *str = TxGetLn(Ttxt, &len);
+    x = vipFind(str, len, (x < 0) ? len : x-1, TRUE);
     if (x != -1) {
       Ty = Ttxt->txy;
       Tx = x; return;

twm.cpp
@@ -410,18 +410,20 @@ void tmFentr (void)                   /* file name ENTeR -- ввести имя 
   LenterARG(filebuf, &filebuflen, LFPROMPT, fileHistory,
                      &filebufFlag, TM_F1ENTR, TM_F2ENTR, 0);
 }
-void tmDoFentr (void) { if (filebuflen > LFPROMPT)
-                          twDirPush(tcs2qstr(filebuf   +LFPROMPT,
-                                             filebuflen-LFPROMPT)); }
-void tmDoFentr2 (void)
-{ 
-  if (filebuflen > LFPROMPT) {
-    wnd *wind = vipSplitWindow(Twnd, TM_VFORK);
-    if (wind) twEdit(wind, tcs2qstr(filebuf   +LFPROMPT,
-                                    filebuflen-LFPROMPT), NULL, true);
-} }
-void twShowFile(QString name) { wnd *wind = vipSplitWindow(Twnd, TM_VFORK);
-                                if  (wind)  twEdit(wind, name, NULL, true); }
+QString tmFentrFN (void)
+{
+  QString fn = tcs2qstr(filebuf+LFPROMPT, filebuflen-LFPROMPT);
+  fn.replace(QString::fromUtf8("•"), Ttxt->file->name);
+  return fn; //                 ^
+}            // attrs stripped already, but this char not common in filenames
+/*- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -*/
+void tmDoFentr (void) { if (filebuflen > LFPROMPT)  twDirPush(tmFentrFN()); }
+void tmDoFentr2(void) { if (filebuflen > LFPROMPT) twShowFile(tmFentrFN()); }
+void twShowFile(QString name)
+{
+  wnd *wind = vipSplitWindow(Twnd, TM_VFORK);
+  if  (wind)  twEdit(wind, name, NULL, true);
+}
 /*---------------------------------------------------------------------------*/
 static void tmExtractIncs (txt *mcd, QStringList& incList)
 {

tx.c
@@ -204,6 +204,7 @@ void TxTRep (txt *t, tchar *tp, small len)
 }
 void TxFRep (txt *t, tchar *tp) { TxTRep(t, tp, lstrlen(MAXLPAC, tp)); }
 /*- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -*/
+char *TxGetLn(txt *t, int *len) { return DqLookupForw(t->txdstk,0, len,0); }
 small TxRead (txt *t, char *tp)
 {
   return qTxBottom(t) ? 0 : DqCopyForward(t->txdstk,0, tp,0);

tx.h
@@ -15,6 +15,7 @@ void TxEnableSynt(txt *t, small clang);
 small aftotc (const char *orig, int len, tchar *dest_buf);
 small tctoaf (tchar      *orig, int len, char  *dest_buf);
 /*- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -*/
+char *TxGetLn(txt *t, int *len);     /* get top (raw) line from upper stack  */
 small TxRead (txt *t,  char *p);     /* read text line into specified buffer */
 small TxTRead(txt *t, tchar *p);     /*                                      */
 small TxFRead(txt *t, tchar *p);     /* <- fill with spaces up to MAXLPAC    */

vip.cpp
@@ -477,9 +477,9 @@ void vipPrepareSearch()             /* prepare Qspre/Qsstr for the vipFind() */
       (find_Mode & LeARG_WILDCARD)   ? QRegExp::Wildcard   : QRegExp::RegExp);
 }
 /*- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -*/
-int vipFind (tchar *str, int st_len, int from_pos, BOOL backward)
+int vipFind (char *str, int st_len, int from_pos, BOOL backward)
 {
-  QString line = tcs2qstr(str, st_len);
+  QString line = QString::fromUtf8(str, st_len);
   Qt::CaseSensitivity cs = (find_Mode & LeARG_IGNORECASE) ? Qt::CaseInsensitive
                                                           : Qt::CaseSensitive;
   if (backward) {

vip.h
@@ -91,7 +91,7 @@ BOOL   TxSetY(txt *t, large y);
 tchar *TxInfo(wnd *w, large y, int *len);                 /* defined in tx.c */
 /*- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -*/
 void vipPrepareSearch(); /* using patterns prepared by tesParse() - see te.h */
-int  vipFind(tchar *str,
+int  vipFind(char  *str,
              int st_len, int from_pos, BOOL backward);
 /*
  *  Next function verifies that 'str' matches previously set pattern at given