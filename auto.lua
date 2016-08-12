epiktetov/microMup07

commit 04207eaaea4bc45552b8ca6786e57411c8422bcc

More markers with text, for compiler diagnostics and such
may be set from Lua by Tx:mark(k,x,y,”text”), shows the text in a window  
at the top; MkSyncMark script called from tmSyncPos command to add marks
to the file being synced; fixed grep in current file using “string/•”

auto.lua
@@ -79,6 +79,24 @@ do local function breakable(c) return (c == ' ' or c == ',' or c == ';') end
                        while Tx.reX do BlockFormat(Tx) end end
   end
 end
+function MkSyncMarks(Rx,Tx)
+  local ref = Re[[([+./0-9A-z-]+):(\d+):((\d+):)?(.*)]]
+  local Y,N = Rx.Y,4;     local Tname
+  if ref:ifind(Rx:line(Y)) then Tname = ref:cap(1) else return end
+  -- print("MkSyncMarks(Tname="..Tname..",Y="..Y..",maxY="..Rx.maxY..")")
+  repeat
+    -- print("["..Y.."] "..Rx:line(Y))
+    if ref:ifind(Rx:line(Y)) then
+      local name,y,_,x,text = ref:caps()
+      -- print("name="..name..",x="..x..",y="..y..",text="..text)
+      if name == Tname then
+        x = tonumber(x) or 2
+        y = tonumber(y) or 0; Tx:mark(N,x,y,text)
+        N = N+1;       if N == 20 then return end
+      end
+    end Y = Y+1
+  until(Y > Rx.maxY)
+end
 function Mk2html(Tx)        -- convert MicroMir text (with ʁboldʀ etc) to HTML
   local Hx = Txt.open(true) --
   Hx:IL("auto-generated from `"..Tx.name.."` at "..os.date())

luas.cpp
@@ -1,5 +1,5 @@
 /*------------------------------------------------------+----------------------
-// МикроМир07          Embedded Lua scripting           | (c) Epi MG, 2011-2014
+// МикроМир07          Embedded Lua scripting           | (c) Epi MG, 2011-2016
 //------------------------------------------------------+--------------------*/
 #include <QRegExp>
 #include "mim.h"
@@ -126,6 +126,9 @@ void luasNtxt (txt *newTxt)  // "new text" hook (called from tmDoLoad, twm.cpp)
   luaP_getmetatable("txt");            //
   luaQ_setmetatable(-2);               // NOTE: metatable for Tx obj must be
   luaQQ_rawset   (iTxt); luaQn_pop(1); // set after Tx.id (to avoid recursion)
+//+
+//  fprintf(stderr, "luasNtxt(%s):%d\n", newTxt->file->full_name.cStr(),
+//                                       newTxt->luaTxid);
 }
 //- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 static txt *luasN_gettext (int ix)  // returns txt from Lua reference on given
@@ -287,6 +290,8 @@ static int luTxGo (lua_State *L) // Tx:go([dx,]dy) == Tx.go(Tx,[dx,]dy)
 //   Tx:IL("line") = insert given line at cursor   | inserted text
 //   Tx:DC(N) = delete N characters at cursor
 //   Tx:DL(N) = delete N lines at cursor (default = 1)
+//   Tx:gtl() = get-text-left   (gets first chunk of non-spaces left of cursor)
+//   Tx:mark(k,x,y,"text") = add/update mark k (0…19) at x,y (y<0 to remove it)
 //- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 static int luTxIC (lua_State *L) // Tx:IC("text") = insert given text at cursor
 {
@@ -350,9 +355,27 @@ int luGetTextLeft (lua_State*) // Tx:gtl() = get first chunk of non-spaces from
   else         { luaP_pushnil();             return 0; }
 }
 //- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+int luTxMark (lua_State*) // Tx:mark(k,x,y,"text") =  add/update mark k (0…19)
+{                         //   at given position, or remove the mark when x=y=0
+  txt *t = luasN_gettext(1);
+  int  k = luaL_checkinteger(L,2); if (k < 0 || k >= TXT_MARKS) return 0;
+  int  x = luaL_checkinteger(L,3); if (x > MAXLPAC-3)      x = MAXLPAC-3;
+  int  y = luaL_optinteger(L,4,0); if (y >  t->maxTy)      y =  t->maxTy;
+//
+  if (t->txmarks[k]) xfree(t->txmarks[k]); // clean up the old text in any case
+  t->txmarkx[k] = x ? x-1  : 0;            // (it will either remain 0, or gets
+  t->txmarky[k] =     y-1;                 //          replaced with new value)
+  t->txmarks[k] = 0;
+  if (x > 0 && y > 0) {                    size_t len;
+    const char *text = luaL_optlstring(L,5,NULL,&len);
+    if (text)     t->txmarks[k] = xstrndup(text, len);
+  }
+  wndop(TW_ALL, t); return 0; // always redraw the text (inefficient, but we're
+}                             //  not going to check if mark is visible or not)
+//- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 luaL_Reg luTxFuncs[] =
-{                         { "go", luTxGo }, { "gtl", luGetTextLeft },
-  { "open",  luTxOpen  }, { "IL", luTxIL },
+{                         { "go", luTxGo }, { "gtl",  luGetTextLeft },
+  { "open",  luTxOpen  }, { "IL", luTxIL }, { "mark", luTxMark      },
   { "focus", luTxFocus }, { "IC", luTxIC },
   { "line",  luTxLine  }, { "DL", luTxDL },
   { "lines", luTxLines }, { "DC", luTxDC }, { NULL, NULL   }
@@ -456,6 +479,16 @@ int luasFunc (void)       // executing Lua function (from the top of Lua stack)
     vipError(QString("LuaERROR: %1").arg(lua_tostring(L,-1)));
     luaQn_pop(1);                              return E_SFAIL;
 } }
+//- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+void luas2txtProc (const char *proc_name, txt *Ref, txt *Txt)
+{
+  luaP_getglobal(proc_name);
+  luaP_getglobal("Txt"); luaX_rawgeti_top(Ref->luaTxid);
+  luaP_getglobal("Txt"); luaX_rawgeti_top(Txt->luaTxid);
+  int rc = luaQX_pcall(2,0);
+  if (rc)
+    vipError(QString("LuaERROR: %1").arg(lua_tostring(L,-1)));
+}
 //------------------------------------------------------//---------------------
 static txt *tLF = NULL;                                 // --- Load Filters ---
 static int luLF_addText(lua_State*)                     //---------------------

tx.c
@@ -1,5 +1,5 @@
 /*------------------------------------------------------+----------------------
-// МикроМир07              Texts - Тексты               | (c) Epi MG, 2007-2012
+// МикроМир07              Texts - Тексты               | (c) Epi MG, 2007-2016
 //------------------------------------------------------+--------------------*/
 #include "mic.h"             /* Old tx.c (c) Attic 1989, (c) EpiMG 1998,2001 */
 #include "qfs.h"
@@ -26,14 +26,15 @@ char txbuf[MAXLUP+2];
  */
 void TxInit() { blktspac(tcbuf, MAXLPAC); }
 /*- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -*/
-txt *TxNew (bool qundo)                        /* Создание / удаление текста */
-{
+txt *TxNew(bool qundo) /* Создание текста / удаление оного, точнее - очистка */
+{                      /* (структура переиспользуется, никогда не удаляется) */
   txt *t, *tprev;
   for (tprev = NIL, t = texts; t && t->txstat != 0; t = t->txnext) tprev = t;
   if (t == NIL) {
-    t = (txt*)GetMain(sizeof(txt)); /* если не нашли свободного дескриптора: */
-    t->txstat = 0;     t->file = 0; /* сделаем новый, добавим в конец списка */
-    t->txnext = NIL;                /* (не было предыдущего = список пустой) */
+    t = (txt*)xmalloc(sizeof(txt)); /* если не нашли свободного дескриптора: */
+    memset(t,   0,    sizeof(txt)); /* сделаем новый, добавим в конец списка */
+    t->txstat = 0;     t->file = 0; /* (не было предыдущего = список пустой, */
+    t->txnext = NIL;                /*         добавим ссылку на этот текст) */
     if (tprev) tprev->txnext = t;
     else               texts = t;
   }
@@ -45,10 +46,9 @@ txt *TxNew (bool qundo)                        /* Создание / удале�
   t->clang  = 0;
   t->vp_ctx = t->vp_wtx = 0; t->txy = t->maxTy = 0;
   t->vp_cty = t->vp_wty = 0;
-  memset(t->thisSynts, 0xDE, sizeof(int) * MAXSYNTBUF);
+  memset(t->thisSynts, 0xDE, sizeof(int) * MAXSYNTBUF); t->luaTxid = 0;
   memset(t->prevSynts,    0, sizeof(int) * MAXSYNTBUF);
-  memset(t->lastSynts,    0, sizeof(int) * MAXSYNTBUF);
-  t->luaTxid = 0;                             return t;
+  memset(t->lastSynts,    0, sizeof(int) * MAXSYNTBUF);       return t;
 }
 /*- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -*/
 void TxEnableSynt (txt *t, short clang)       /* add deqs for syntax checker */
@@ -58,19 +58,25 @@ void TxEnableSynt (txt *t, short clang)       /* add deqs for syntax checker */
     t->cldstk = DqNew(DT_SYNT, MAXLPAC, 0);
 } }
 /*- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -*/
-void TxMarks0(txt *t) { int i;
-                        for (i=0; i<TXT_MARKS; i++) { t->txmarkx[i] =  0;
-                                                      t->txmarky[i] = -1; }}
-void TxDel (txt *t)
+void TxMarks0 (txt *t)        /* clear (set to zero) all marks in given text */
 {
-  t->txstat = 0; DqDel(t->txustk); t->txustk = NIL; QfsClear(t->file);
-  t->maxTy  = 0; DqDel(t->txdstk); t->txdstk = NIL;          t->file = NIL;
-  if (t->txudeq) DqDel(t->txudeq); t->txudeq = NIL;
-  if (t->clustk) DqDel(t->clustk); t->clustk = NIL; t->luaTxid = 0;
-  if (t->cldstk) DqDel(t->cldstk); t->cldstk = NIL; t->clang   = 0;
+  for (int i = 0; i < TXT_MARKS; i++) TxUnmark(i, t);
+}
+void TxUnmark (int i, txt *t) {            t->txmarkx[i] =   0;
+                                           t->txmarky[i] =  -1;
+  if (t->txmarks[i]) xfree(t->txmarks[i]); t->txmarks[i] = NIL;
 }
 /*- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -*/
-void TxDiscard (txt *t) /* Discard the content of the file, also UNDO & Synt */
+void TxDel (txt *t) /* Delete the contents (do not de-allocate 'txt' itself) */
+{
+  t->txstat = 0; DqDel(t->txustk); t->txustk = NIL; QfsClear(t->file);
+  t->maxTy  = 0; DqDel(t->txdstk); t->txdstk = NIL; t->file =  NIL;
+  if (t->txudeq) DqDel(t->txudeq); t->txudeq = NIL; t->clang =   0;
+  if (t->clustk) DqDel(t->clustk); t->clustk = NIL; t->luaTxid = 0;
+  if (t->cldstk) DqDel(t->cldstk); t->cldstk = NIL;    TxMarks0(t);
+}
+/*- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -*/
+void TxDiscard (txt *t) /* Discard the contents of the file, and UNDO & Synt */
 {
   DqEmpt(t->txdstk); if (t->clustk) DqEmpt(t->clustk);
   DqEmpt(t->txustk); if (t->cldstk) DqEmpt(t->cldstk);
@@ -81,9 +87,9 @@ void TxDiscard (txt *t) /* Discard the content of the file, also UNDO & Synt */
   t->txstat  &= ~TS_FILE;
   memset(t->thisSynts, 0, sizeof(int) * MAXSYNTBUF);
   memset(t->prevSynts, 0, sizeof(int) * MAXSYNTBUF);
-  memset(t->lastSynts, 0, sizeof(int) * MAXSYNTBUF);
+  memset(t->lastSynts, 0, sizeof(int) * MAXSYNTBUF); TxMarks0(t);
 }
-void TxEmpt(txt *t) { TxDiscard(t); TxMarks0(t); wndop(TW_EM, t); } /* Empty */
+void TxEmpt(txt *t) { TxDiscard(t); wndop(TW_EM,t); } /* make the text Empty */
 /*---------------------------------------------------------------------------*/
 bool qTxBottom(txt *t) { return qDqEmpt(t->txdstk); }
 bool qTxTop   (txt *t)
@@ -145,11 +151,15 @@ void TxDL(txt *t)
   if (qTxBottom(t)) exc(E_MOVDOWN);
   else {
     short len = DqGetB(t->txdstk, txbuf); tundo1add(t, UT_DL, txbuf, len);
-    if (t->cldstk &&
-      !qDqEmpt(t->cldstk)) DqGetB(t->cldstk, (char*)(t->thisSynts));
+    if (t->cldstk && !qDqEmpt(t->cldstk))
+                       DqGetB(t->cldstk, (char*)(t->thisSynts));
     int i;
-    for (i=0; i<TXT_MARKS; i++) if (t->txmarky[i] > t->txy) t->txmarky[i]--; 
-    t->maxTy--;                                             wndop(TW_DL, t);
+    for (i=0; i<TXT_MARKS; i++) {
+      if (t->txmarky[i] < t->txy) continue;
+      if (t->txmarky[i] > t->txy) t->txmarky[i]--;
+      else                        TxUnmark (i, t);
+    }
+    t->maxTy--; wndop(TW_DL, t);
 } }
 /*- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -*/
 void TxDEL_beg(txt *t)
@@ -165,19 +175,18 @@ void TxDEL_beg(txt *t)
   if (t->clustk && !qDqEmpt(t->clustk)) DqEmpt(t->clustk);
   for (i=0; i<TXT_MARKS; i++) {
     if (t->txmarky[i] > num_deleted) t->txmarky[i] -= num_deleted;
-    else                             t->txmarky[i]  = -1;
+    else                                           TxUnmark(i, t);
 } }
 /*- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -*/
 void TxDEL_end(txt *t)
 {
   t->maxTy = t->txy;
-  int i;
   while (!qDqEmpt(t->txdstk)) {
     short len = DqGetB(t->txdstk, txbuf);
     tundo1add(t, UT_DL, txbuf, len);
   }
   if (t->cldstk && !qDqEmpt(t->cldstk)) DqEmpt(t->cldstk);
-  for (i=0; i<TXT_MARKS; i++)
+  for (int i=0; i<TXT_MARKS; i++)
     if (t->txmarky[i] > t->txy) t->txmarky[i] = -1; /* deleting end-of-text  */
 }                                                   /* may delete some marks */
 /*- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -*/
@@ -189,8 +198,8 @@ void TxIL(txt *t, char *text, short len)
     int clen = SyntParse(t, text, len, t->thisSynts) * sizeof(int);
     DqAddB (t->cldstk,         (char*)(t->thisSynts),        clen);
   }
-  int i; for (i=0; i<TXT_MARKS; i++)
-           if (t->txmarky[i] >= t->txy) t->txmarky[i]++;  wndop(TW_IL, t);
+  for (int i=0; i<TXT_MARKS; i++)
+    if (t->txmarky[i] >= t->txy) t->txmarky[i]++;  wndop(TW_IL, t);
 }
 void TxTIL (txt *t, tchar *tp, short len)
 {
@@ -199,7 +208,9 @@ void TxTIL (txt *t, tchar *tp, short len)
 /*- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -*/
 void TxRep (txt *t, char *text, short len)
 {
-  if (!qTxBottom(t)) {
+  if (!qTxBottom(t)) {                  // delete mark (4+) in the edited line
+    for (int i = 4; i < TXT_MARKS; i++) //      (likely not applicable anymore)
+      if (t->txmarky[i] == t->txy) TxUnmark(i,t);
     short olen = DqGetB(t->txdstk, txbuf);
     tundo2add(t,  txbuf, olen, text, len);
     DqAddB   (t->txdstk,       text, len); wndop(TW_RP, t);
@@ -361,22 +372,25 @@ tchar txFlags[TXT_MARKS] = { AT_MARKFLG             + 0xB0,   /* brown °     */
 /*- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -*/
 tchar *TxInfo (wnd *w, long y, int *pl)       /* used for repaint in vip.cpp */
 {                                             /* to get current char mim.cpp */
-  txt *t = w->wtext;                          /* (return buf may be changed) */
-  int len = 0;
+  int len = 0, i, x, attr;                    /* (return buf may be changed) */
+  txt *t = w->wtext;
   if (t && TxSetY(t, y)) {
     if (w == Lwnd && y == Ly) blktmov(Lebuf, tcbuf, len = Lleng);
     else                                 len = TxTRead(t, tcbuf);
+    if (len < tcbuflen)    blktspac(tcbuf + len, tcbuflen - len);
     if (t->maxTy < y) t->maxTy = y;
-    if (len < MAXLPAC-3) {
-      int i;
-      for (i = 0; i < TXT_TempMARK; i++) {
-        if (t->txmarky[i] == y) {
-          if (len < w->wsw-1) tcbuf[len++] = ' ';
-                              tcbuf[len++] =           txFlags[i];
-                              tcbuf[len++] = ' '|tATTR(txFlags[i]); break;
-  } } } }
-  if (len < tcbuflen) blktspac(tcbuf+len, tcbuflen - len);
-  *pl = tcbuflen = len;                      return tcbuf;
+    for (i = 1; i < TXT_MARKS; i++) { if (t->txmarky[i] != y) continue;
+                                      x = t->txmarkx[i];
+      if (i < 4)
+      { tcbuf[x] = txFlags[i]; attr = tATTR(txFlags[i]); }
+      else  if (t->txmarks[i]) attr = tATTR(txFlags[1]);
+      else                     attr = tATTR(txFlags[0]);
+      //
+      tcbuf[x] = (tcbuf[x] & AT_CHAR) | attr; x++; if (x > MAXLPAC-3) break;
+      tcbuf[x] = (tcbuf[x] & AT_CHAR) | attr; x++;
+      tcbuf[x] = (tcbuf[x] & AT_CHAR) | attr; x++; if (len < x) len = x;
+  } }
+  *pl = tcbuflen = len; return tcbuf;
 }
 void TxRecalcMaxTy (txt *t) { TxBottom(t); t->maxTy = t->txy; }
 /*---------------------------------------------------------------------------*/

dq.c
@@ -49,7 +49,7 @@ void DqInit(char *membuf, long bufsize)     /* create "anti-deq" that covers */
 deq *DqNew (short typ, short bext, short eext)
 {
   deq *d; if (freedeqs) { d = freedeqs; freedeqs = d->dnext; }
-          else            d = (deq*)GetMain(sizeof(deq));
+          else            d = (deq*)xmalloc(sizeof(deq));
   deq *dn = deq0.dprev;
   d->dprev = dn;
   dn->dnext = d;                    d->dtyp  = typ;

luas.h
@@ -1,5 +1,5 @@
 /*------------------------------------------------------+----------------------
-// МикроМир07          Embedded Lua scripting           | (c) Epi MG, 2011,2014
+// МикроМир07          Embedded Lua scripting           | (c) Epi MG, 2011,2016
 //------------------------------------------------------+--------------------*/
 #ifndef LUAS_H_INCLUDED
 #define LUAS_H_INCLUDED
@@ -9,6 +9,11 @@ void luasNtxt(txt*newTxt); /*  new-text hook (called from tmDoLoad, twm.cpp) */
 int  luasExec(txt*, bool); /*  load/execute given text (current line) as Lua */
 int  luasFunc(void); /* execute (and pop) Lua function from top of Lua stack */
 int luLF_read(txt*); /* read txt->file using load filter named txt->file->lf */
+//
+// Method to call Txt Processor in Lua (currently used only for MkSyncMarks,
+//        that is called from tmSyncPos to sync marks in Txt from Ref text):
+//
+void luas2txtProc(const char *proc_name, txt *Ref, txt *Txt);
 /*- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -*/
 #ifdef lua_h         /* the problem with Lua function names is that it's not */
 extern lua_State *L; /* very clear how do they operate with the stack, fixin */

mic.h
@@ -120,7 +120,8 @@ typedef struct txt_tag txt;     /* twm.h */
         struct wnd_tag;
 typedef struct wnd_tag wnd;     /* twm.h */
 /*---------------------------------------------------------------------------*/
-extern char *GetMain(long n); /* =malloc() but always returns non-NULL, rt.c */
+extern char *xmalloc(long n); /* =malloc() but always returns non-NULL, rt.c */
+extern void xfree(void *mem); /* =free(), only to avoid including <stdlib.h> */
 /*
  * Инициализация "большого" сегмента памяти -- platform-specific manipulations
  * with VirtualAlloc() etc are removed, now it just malloc()'s a bug chunk and
@@ -134,10 +135,11 @@ extern char
   *sncpy(const char *from, char *to, int how_many), /* copy functions return */
   *scpy (const char *from, char *to),               /* pointer to the end of */
   *scpyx(const char *from, char *to, int limit),    /* destination string    */
+  *ltod(char *buffer, long  l),
+  *stod(char *buffer, short s), /* return pointer to end-of-num */
   *dtol(char *pn, long  *dest),
   *dtos(char *pn, short *dest),
-  *ltod(char *buffer, long  l), /* return pointer to end-of-num   */
-  *stod(char *buffer, short s);
+  *xstrndup(const char *, int); /* same as strndup(3) but with our xmalloc() */
 
 char *lblkmov(char *From, char *To, long len); /* returns ptr after the dest */
 void * blkmov(void *From, void *To, long len); /*        block (i.e. To+len) */

micro.keys
@@ -539,11 +539,13 @@ NOTE:
 
   Tx:IC("text") -- insert text into current line | moves cursor past
   Tx:IL("line") -- insert given line at cursor   |     inserted text
-  Tx:DC(N) -- delete N characters at cursor
+
+  Tx:mark(k,x,y,"text") -- add/update mark k (0…19) at given position (use x=0
+  Tx:DC(N) -- delete N characters at cursor                     to remove mark)
   Tx:DL(N) -- delete N lines at cursor (default = 1)
   Tx:gtl() -- get-text-left convenience method = gets first chunk of non-spaces
                                        from text to the left of cursor position
-[line 541] xref: new.lua
+[line 548] xref: new.lua
 
   re = Re("..regex..",true/false) -- regular expression (arg: case sensitivity,
   re = Re[[..regex..]]                                default = case-sensitive)

micros.dir
@@ -23,7 +23,7 @@ Mac-specific code (GCC's Objectvie-C++ extension)              |macs.h
 Shell commands and tmSyncPos (formerly UNIX-specific)          |unix.h
                                                                |unix.cpp
 - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -|- - - - - - - -
-ViewPort (interface between wx/C++ and legacy C code) and some |vip.h
+ViewPort (interface between Qt/C++ and legacy C code) and some |vip.h
      other stuff used in both C and C++ code (like QString ops)|vip.cpp
 ---------------------------------------------------------------|---------------
 Qt project file, adds microMir.icns micros.icns micons.psd etc |qtmim.pro

commit 51ab0341e9c1f1b370c5914419afe39951965156

fixed TxInfo (buf cleanup broken in 04207ea), added arg Tx:lines(fromY)

auto.lua
@@ -27,30 +27,28 @@ Mk["Ctrl+X"] = function(Tx)                   --     end
   for i=Tx.Y,1,-1 do
     if Tx.refs[i] then Rx:focus(); Rx.Y = Tx.refs[i]; return end
   end
-end                         -- called from (tm)SyncPos
-function MkSyncMarks(Rx,Tx) --
+end
+function MkSyncMarks(Rx,Tx) -- called from (tm)SyncPos
   local ref = Re[[([+./0-9A-z-]+):(\d+):((\d+):)?(.*)]]
-  local gre = Re"grep -\\w+ \'([^\\']+)\'"
-  local Y,N = Rx.Y,4;     local Tname,grept
-  if ref:ifind(Rx:line(Y)) then Tname = ref:cap(1) else return end
-  if gre:ifind(Rx:line(1)) then grept = gre:cap(1)             end
+  local gre = Re"grep -\\w+ \'([^\\']+)\'"; local N = 4
+  local Tname, grept
+  if ref:ifind(Rx:line(Rx.Y)) then Tname = ref:cap(1) else return end
+  if gre:ifind(Rx:line(1))    then grept = gre:cap(1)             end
   Tx.refTx = Rx.id;Tx.refs = { }
-  repeat
-    if ref:ifind(Rx:line(Y)) then
-      local name,y,_,x,text = ref:caps()
-      if name == Tname and tonumber(y) then
-        if grept  and  not tonumber(x) then
-          local Ts = Tx:line(tonumber(y))
-          x = Ts and Ts:find(grept)
-          if x then text = "" end
-        elseif text:find("error:") then text = "!"..text
-        elseif text:find("note:")  then text = "@"..text end
-        Tx.refs[tonumber(y)] = Y
-        Tx:mark(N, tonumber(x) or 2, y, text)
-        N = N+1; if N == 20 then return end
-      end
-    end Y = Y+1
-  until(Y > Rx.maxY)
+  for Y,line in Rx:lines(Rx.Y) do
+    if ref:ifind(line) then
+        local name,y,_,x,text = ref:caps()
+        if name == Tname and tonumber(y) then
+          if grept  and  not tonumber(x) then
+            local Ts = Tx:line(tonumber(y))
+            x = Ts and Ts:find(grept); if x then text = "" end
+          elseif text:find("error:")        then text = "!"..text
+          elseif text:find("note:")         then text = "@"..text end
+          Tx.refs[tonumber(y)] = Y
+          Tx:mark(N, tonumber(x) or 2, y, text)
+          N = N+1;   if N == 20 then return end
+    end end
+  end
 end
 do local function breakable(c) return (c == ' ' or c == ',' or c == ';') end
    local function squeeze(line,afterN)

luas.cpp
@@ -216,12 +216,13 @@ static int luTxLine_it (lua_State *L)   // line iterator function called as:
   }
   else { luaP_pushnil(); luaP_pushnil(); }  return 2;
 }
-static int luTxLines (lua_State *)
-{
-  luasN_gettext(1);
-  luaP_pushCfunction(luTxLine_it); // function = luTxLine_it(self,N)
-  luaP_pushvalue  (1);             //    state = text descriptor
-  luaP_pushinteger(0); return 3;   // init.var = 0 (before 1st line)
+static int luTxLines (lua_State *)  // optional parameter to Tx:lines specifies
+{                                   // start position for iterator (default: 1)
+  luasN_gettext(1);                 //
+  long Y = (long)luaL_optnumber(L,2,1);
+  luaP_pushCfunction(luTxLine_it);  //    function = luTxLine_it(self,N)
+  luaP_pushvalue    (1);            //       state = text descriptor
+  luaP_pushinteger(Y-1); return 3;  //    init.var = Y-1 (before given line)
 }
 //- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 static int luTxIndex (lua_State *L) // Tx.X and Tx.Y - read/write access

tx.c
@@ -373,11 +373,12 @@ tchar *TxInfo (wnd *w, long y, int *pl)       /* used for repaint in vip.cpp */
 {                                             /* to get current char mim.cpp */
   int len = 0, i, x;                          /* (return buf may be changed) */
   txt *t = w->wtext;
-  if (t && TxSetY(t, y)) {
-    if (w == Lwnd && y == Ly) blktmov(Lebuf, tcbuf, len = Lleng);
-    else                                 len = TxTRead(t, tcbuf);
-    if (len < tcbuflen)    blktspac(tcbuf + len, tcbuflen - len);
-    if (t->maxTy < y) t->maxTy = y;
+  if (w == Lwnd && y == Ly) blktmov(Lebuf, tcbuf, len = Lleng );
+  else if (t && TxSetY(t,y)) {
+    if (t->maxTy < y) t->maxTy = y;     len = TxTRead(t, tcbuf);
+  }
+  if (len < tcbuflen) blktspac(tcbuf+len, tcbuflen-len);
+  if (t) {
     for (i = 1; i < TXT_MARKS; i++) { if (t->txmarky[i] != y) continue;
                                       x = t->txmarkx[i];
     //
@@ -386,8 +387,8 @@ tchar *TxInfo (wnd *w, long y, int *pl)       /* used for repaint in vip.cpp */
     //
        tchar attr = tATTR(t->txmarkt[i]);
        if (tcharIsBlank(tcbuf[x])) tcbuf[x] = t->txmarkt[i];
-       else                        tcbuf[x] = (tcbuf[x] & AT_CHAR) | attr; x++;
-       if (x > MAXLPAC-2) break;   tcbuf[x] = (tcbuf[x] & AT_CHAR) | attr; x++;
+       else                        tcbuf[x] = (tcbuf[x] & AT_CHAR)|attr; x++;
+       if (x < MAXLPAC-1)        { tcbuf[x] = (tcbuf[x] & AT_CHAR)|attr; x++; }
        if (len < x) len = x;
   } }
   *pl = tcbuflen = len; return tcbuf;