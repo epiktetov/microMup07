epiktetov/microMup07

commit f9130ebee2edb80ba65e7bd92a92edd2d689d4cc

Restored Txt.this, Mk:XEQ -> Mk:Do, fixed Lua err msg, added README.html
Restored Txt.this (so one-line scripts has some reference); renamed Mk:XEQ
to simpler Mk:Do; Lua error message for "just1line case fixed; added func
Mk2html(Tx) to auto.lua, and README.html (generated from micro.keys)

auto.lua
@@ -79,3 +79,20 @@ do local function breakable(c) return (c == ' ' or c == ',' or c == ';') end
                        while Tx.reX do BlockFormat(Tx) end end
   end
 end
+function Mk2html(Tx)        -- convert MicroMir text (with ʁboldʀ etc) to HTML
+  local Hx = Txt.open(true) --
+  if Hx.maxY > 0 then Hx.Y = 1; Hx:DL(Hx.maxY) end
+  Hx:IL[[<html><head>]]
+  Hx:IL[[<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">]]
+  Hx:IL[[</head><body>]]
+  Hx:IL[[<span style="font-family:Consolas,Liberation Mono,Menlo,monospace">]]
+  Hx:IL[[<font size="3">]]
+  for N,line in Tx:lines() do
+    line = line:gsub("<(.-)>","&lt;%1&gt;")
+    line = line:gsub("\202\129(.-)\202\128","<b>%1</b>")
+    line = line:gsub("\202\129(.+)","<b>%1</b>")
+    line = line:gsub("  ","&nbsp;&nbsp;")
+    Hx:IL(line.."<br>")
+  end
+  Hx:IL[[</font></span>]]; Hx:IL[[</body></html>]]
+end

micro.keys
@@ -492,14 +492,16 @@ MicroMir-specific Lua objects and operations
   Mk["keysequence"] = function(Tx,count)   -- map Lua function to key sequence
     ...
   end
-  Mk:XEQ( Mk["keysequence"] ) -- execute given command (or text with commands),
-  Mk:XEQ("text-and-commands") --      as if they were entered from the keyboard
+  Mk:Do( Mk["keysequence"] ) -- execute given command (or text with commands),
+  Mk:Do("text-and-commands") --      as if they were entered from the keyboard
 
   Tx = Txt.open("filename") -- create/open given file in new window
   Tx = Txt.open(true)       -- open new window with unnamed file (saveable)
   Tx = Txt.open(false)      -- open new window with throw-away text
-
-  Tx.id -- text ID for safe reference, Txt[Tx.id] -> Tx or nil if text replaced
+  Tx = Txt.this             -- reference to "this" text (may be used only from
+                                    text with script iself, not from functions)
+  Tx.id -- text ID = index in Txt
+  (should be used for safe reference, Txt[Tx.id] -> Tx or nil if text replaced)
 
   Tx:focus() -- focus last opened window, associated with the text
   Tx:line(N) -- content of Nth line in given text (= nil if past end-of-text)
@@ -520,7 +522,7 @@ NOTE:
   Tx:DL(N) -- delete N lines at cursor (default = 1)
   Tx:gtl() -- get-text-left convenience method = gets first chunk of non-spaces
                                        from text to the left of cursor position
-[line 523] xref: new.lua
+[line 525] xref: new.lua
 
   re = Re("..regex..",true/false) -- regular expression (arg: case sensitivity,
   re = Re[[..regex..]]                                default = case-sensitive)
@@ -543,6 +545,7 @@ Examples (auto-loaded at startup):
                               else Tx:IL(pref..'none') end
     end
   end
+  Mk2html(Tx.this) -- convert this text to HTML (use Ctrl+Shift+J in this line)
 */
 k_(TK_SM0,       NULL, 0xfd235) /* ʁMACRO:ʀ In addition to universal scripts, */
 k_(TK_SM1, "Shift+F7", 0xfd236) /* MikroMir provides quick method to record */

ccd.cpp
@@ -211,7 +211,7 @@ void MkLuaXEQ (QString text, wnd *vp) // text is only used when stack[-1] = nil
   if (!text.isEmpty()) MkStrXEQ(text,vp);        luaQn_pop(2);
 }
 static int luMkXEQ (lua_State *L) // this function is called from Lua scripts
-{                                 // whenever explicit Mk:XEQ(something) used
+{                                 // whenever explicit Mk:Do(something) used;
   luaL_checktype(L,1,LUA_TTABLE); // no return value, but may raise an error
   luaL_checkany (L,2);
   MkLuaXEQ ("", Twnd); if (qkbhin()) { luaP_pushinteger(1);
@@ -251,7 +251,7 @@ void MkInitCCD (void)  // moves Command Codes Definitions into global Lua table
     if (pt->kseq) { luaP_pushstring (pt->kseq);
                     luaP_pushinteger(pt->hexcode); luaQQ_rawset(-3); }
   }
-  luaP_pushstring     ("XEQ");
+  luaP_pushstring      ("Do");
   luaP_pushCfunction(luMkXEQ); luaQQ_rawset(-3); luaQ_setglobal("Mk");
 }
 //-----------------------------------------------------------------------------

luas.cpp
@@ -364,7 +364,8 @@ static void luTxtInit (void)
 // Txt table
 //   open(tn/f) - object constructor, by text name or bool flag (true/false)
 //   functions  - instance functions (methods == line,lines,go,IC,IL,DC,DL)
-//   [luaTxid]  - active texts are stored here (idexed by txt->luaTxid)
+//   this       - reference to the currently active text (Ttxt), luasExec only
+//   [luaTxid]  - all active texts are stored here (idexed by txt->luaTxid)
 //     [k].id   -- instance id as userdata == { &txt_tag, txt_tag.luaTxid }
 // metatable for instance objects
 //   __index    - read access to instance variables (X,Y,maxY;ref->Txt)
@@ -373,16 +374,14 @@ static void luTxtInit (void)
   luaP_newlibtable(luTxFuncs); luaX_setfuncs(luTxFuncs); luaQ_setglobal("Txt");
   luaP_newmetatable   ("txt"); luaX_setfuncs(luTxMetaFuncs);      luaQn_pop(1);
 }
-//static void luasN_setTxt_this (txt *Tx) //- - - - - - - - - - - - - - - - - -
-//{
-//  luaP_getglobal  ("Txt"); // Txt.this = Txt[ Ttxt->luaTxid ] or nil (clear)
-//  luaP_pushstring("this"); //
-//  if (Tx) luaP_rawgeti(-2,Tx->luaTxid);
-//  else    luaP_pushnil();                  removed - Sun Oct 21 12:07:39 2012
-//          luaQQ_rawset(-3); luaQn_pop(1);  (do not remmeber why it was added,
-//}                                          there seems to be no use for that)
-// Txt table
-//   this       - reference to the currently active text (Ttxt), luasExec only
+static void luasN_setTxt_this (txt *Tx) //- - - - - - - - - - - - - - - - - - -
+{
+  luaP_getglobal  ("Txt"); // Txt.this = Txt[ Ttxt->luaTxid ] or nil (clear)
+  luaP_pushstring("this"); //
+  if (Tx) luaP_rawgeti(-2,Tx->luaTxid);   // to avoid adding metatable to Ttxt
+  else    luaP_pushnil();                 // just to cover one rarely used ref,
+          luaQQ_rawset(-3); luaQn_pop(1); // adding "this" only in the luasExec
+}                                         // scope (^J,^J and Shift^J commands)
 //-----------------------------------------------------------------------------
 void luasInit(void)                            // Lua SCRIPTING initialization
 {
@@ -413,16 +412,17 @@ int luasExec (txt *Tx, bool just1line) // load/execute given text …(tx, false)
     }
     buffer = Tx->txustk->dbeg;
     len    = Tx->txustk->dend - buffer;
-  }                                     // luasN_setTxt_this(Tx);
+  }                                        luasN_setTxt_this(Tx);
   int rc = luaL_loadbuffer(L, buffer, len, Tx->file->name.uStr())
-              || lua_pcall(L,0,0,0);  // luasN_setTxt_this(NULL);
+              || lua_pcall(L,0,0,0);     luasN_setTxt_this(NULL);
   if (rc) {
     const char *luaError = lua_tostring(L,-1);
     QRegExp re("\\[.+\\]:(\\d+):(.+)");
-    QString mimErr;
-    if (re.exactMatch(luaError)) {      TxSetY(Tx, re.cap(1).toInt());
+    if (re.exactMatch(luaError)) {
       QString mimErr = Utf8("^" AF_ERROR "%1" AF_NONE).arg(re.cap(2));
-      TxIL(Tx, mimErr.uStr(), mimErr.length());
+      if (just1line) TxSetY(Ttxt, Ty + 1);
+      else           TxSetY(Tx, re.cap(1).toInt());
+      TxIL (Tx,  mimErr.uStr(),   mimErr.length());
     }
     else vipError(QString("LuaERROR: %1").arg(lua_tostring(L,-1)));
     return E_SFAIL;

new.lua
@@ -1,7 +1,7 @@
---  see :/help, line 523 for ʁLua SCRIPTINGʀ reference (use Meta+Enter to open)
+--  see :/help, line 525 for ʁLua SCRIPTINGʀ reference (use Meta+Enter to open)
 --  :/auto.lua, line 19 for the source of auto-loaded script (incl. openXref)
 --
-Mk['^J,K'] = function(Tx,count)
+Mk['^J,Y'] = function(Tx,count)
   local Rx = Txt.openXref(Tx)
   for N,line in Tx:lines() do
     if ... then

commit 948ecae5d5a3e79c7d411cf1b5877dd40661af42

Fixed TryBlockFormat in auto.lua (broken by f9130ebee 5 years ago)
also added explicit QMAKE_MAC_SDK(DEPLOYMENT_TARGET) = macosx10.11

LICENSE
@@ -2,7 +2,7 @@
  * ----------------------------------------------------------------------------
  * original "MicroMir" idea (c) А.Г.Кушниренко, Г.В.Лебедев (мехмат МГУ)   1981
  * "nanoMir" implementation (c) Attic (Д.В.Варсанофьев, А.Г.Дымченко) 1989-1991
- * "nanoMir" and МикроМир07 (c) EpiMG (М.Г.Эпиктетов)       1998-2001,2006-2017
+ * "nanoMir" and МикроМир07 (c) EpiMG (М.Г.Эпиктетов)       1998-2001,2006-2018
 -------------------------------------------------------------------------------
  Author(s) grant(s) you (''Licensee'') a non-exclusive, royalty free, license
  to use, modify and redistribute this software in source and binary code form,

auto.lua
@@ -88,7 +88,7 @@ do local function breakable(c) return (c == ' ' or c == ',' or c == ';') end
                         Tx.reY      = Tx.Y; BlockFormat(Tx) end
   end
   local function TryBlockFormat(Tx)
-    if Tx.reX then  BlockFormat(Tx) else Mk:XEQ(0xf5035) end
+    if Tx.reX then  BlockFormat(Tx) else Mk:Do(0xf5035) end
   end
   Mk["^J,F6"] = function(Tx,count)
         if           count then Tx.fmtFromX,Tx.fmtToX = 1,count+1

micro.keys
@@ -3,7 +3,7 @@
  * ----------------------------------------------------------------------------
  * original "MicroMir" idea (c) А.Г.Кушниренко, Г.В.Лебедев (мехмат МГУ)   1981
  * "nanoMir" implementation (c) Attic (Д.В.Варсанофьев, А.Г.Дымченко) 1989-1991
- * "nanoMir" and МикроМир07 (c) EpiMG (М.Г.Эпиктетов)   1998-2001 and 2006-2017
+ * "nanoMir" and МикроМир07 (c) EpiMG (М.Г.Эпиктетов)   1998-2001 and 2006-2018
 -------------------------------------------------------------------------------
  Ins is "Help" key on MacPro keyboard, missing on A1243 (see KEY MAPPING below)
  Del is "Delete⌦" on Mac keyboard ("delete" key is Backspace)

micros.dir
@@ -1,4 +1,4 @@
-МикроМир07 © Attic 1989-91, © Epi MG, 1998-2017 -- keycodes -->|micro.keys
+МикроМир07 © Attic 1989-91, © Epi MG, 1998-2018 -- keycodes -->|micro.keys
 - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -|- - - - - - - -
 Самый главный header файл, to be included the first everywhere |mic.h
 Main Header + Scrollable Text Window (ScTwin) class / C++ only |mim.h

qtmim.pro
@@ -20,6 +20,8 @@ macx {
   ICON = microMir.icns
   QMAKE_INFO_PLIST = mim.Info.plist
   QMAKE_PKGINFO_TYPEINFO = "~epi"
+  QMAKE_MAC_SDK            = macosx10.11
+  QMAKE_MACOSX_DEPLOYMENT_TARGET = 10.11
 }
 QMAKE_CFLAGS   += -std=c99
 !win32:DEFINES += UNIX